1f5d795daf6d5a988b300df1e0c0895935abf029
bbjavellana <91865168+bbjavellana@users.noreply.github.com>
2021-11-27 16:13:49 +0800

    Update properties.js

--- properties.js
+++ properties.js
@@ -7,7 +7,7 @@ module.exports = {
   //   "bentestmongodb:pM8wzioeWseuCo0EZI3KIaX5BmihfZuExnfNHxN5H9C4sHIznpIisfOOlxTdQDoIKZCQKyujhJciKpIxt0M7ug==@bentestmongodb.mongo.cosmos.azure.com:10255/eVoucher_db?ssl=true",
   db_dbUrl: (process.env.DB_HOST || 'localhost:27017/bayan_edge_db'),
   publicPath: '../client/build',
-  port: process.env.NODE_PORT || 6010,
+  port: process.env.NODE_PORT || 3001,
   tokenSecret: 'B@y@an3Dge!!M()n3yFl()w',
   api: process.env.NODE_API != null ? process.env.NODE_API : '/api'
 }

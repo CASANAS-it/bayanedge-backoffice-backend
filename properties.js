eedc1489dc208a86255d087a7875084b613ccec4
rmarquez1116 <ronald.marquez111694@gmail.com>
2022-08-15 15:11:33 +0800

    change port

--- properties.js
+++ properties.js
@@ -5,7 +5,7 @@ module.exports = {
   butler_db_dbUrl: (process.env.DB_HOST || '114.198.129.250:27017/bayan_edge_db'),
   butler_db_string: "mongodb://moneyflowAdmin:*m0n3y_flow001!DB@114.198.129.250:27017/bayan_edge_db_training?readPreference=primary&directConnection=true&ssl=false",
   publicPath: '../client/build',
-  port: process.env.NODE_PORT || 3001,
+  port: process.env.NODE_PORT || 8110,
   tokenSecret: 'B@y@an3Dge!!M()n3yFl()w',
   api: process.env.NODE_API != null ? process.env.NODE_API : '/api',
   transporter: {
